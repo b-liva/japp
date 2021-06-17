df8108f0644ff26ef905655f1c3d939e8b323cc1
b-liva <ebammo20@gmail.com>
2021-06-17 15:59:32 +0430

    Add scaffold to home

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Text('First Commit'),
-));
+void main() =>
+    runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('JCM '),
+//      backgroundColor: Colors.blueAccent[200],
+        ),
+        body: Center(
+            child: Text(
+              'میزان فروش',
+              style: TextStyle(
+                fontSize: 20
+              ),
+            )
+        ),
+      ),
+    ));
