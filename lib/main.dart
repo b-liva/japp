4649e2385bdc169aa350ff39bbb3313f79a6d9be
b-liva <ebammo20@gmail.com>
2021-06-19 11:32:01 +0430

    Add monthly stats by project type

--- lib/main.dart
+++ lib/main.dart
@@ -14,28 +14,64 @@ class Home extends StatelessWidget {
         title: Text('JCM '),
 //      backgroundColor: Colors.blueAccent[200],
       ),
-      body: Row(
-        textDirection: TextDirection.rtl,
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+      body: Column(
         children: [
-          Column(
+          Row(
+            textDirection: TextDirection.rtl,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              Text('ماه جاری'),
-              SizedBox(height: 10),
-              Text('12 MW'),
-              SizedBox(height: 10),
-              Text('120000 میلیون ریال'),
+              Column(
+                children: [
+                  Text('ماه جاری'),
+                  SizedBox(height: 10),
+                  Text('12 MW'),
+                  SizedBox(height: 10),
+                  Text('120000 میلیون ریال'),
+                ],
+              ),
+              Column(
+                children: [
+                  Text('سال مالی'),
+                  SizedBox(height: 10),
+                  Text('85 MW'),
+                  SizedBox(height: 10),
+                  Text('600000 میلیون ریال'),
+                ],
+              ),
             ],
           ),
+          SizedBox(height: 40),
           Column(
             children: [
-              Text('سال مالی'),
-              SizedBox(height: 10),
-              Text('85 MW'),
-              SizedBox(height: 10),
-              Text('600000 میلیون ریال'),
+              Row(
+                textDirection: TextDirection.rtl,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text('پروژه'),
+                  Text('6MW'),
+                  Text('65000'),
+                ],
+              ),
+              Row(
+                textDirection: TextDirection.rtl,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text('روتین'),
+                  Text('6MW'),
+                  Text('65000'),
+                ],
+              ),
+              Row(
+                textDirection: TextDirection.rtl,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text('تعمیرات'),
+                  Text('6MW'),
+                  Text('65000'),
+                ],
+              ),
             ],
-          ),
+          )
         ],
       )
     );
