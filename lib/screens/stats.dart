5d54ebd5361e64abca917ae1802d401a6618e16c
b-liva <ebammo20@gmail.com>
2021-06-22 16:18:19 +0430

    Add input (days) to stats

--- lib/screens/stats.dart
+++ lib/screens/stats.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:flutter/services.dart';
 
 class Stats extends StatelessWidget {
   String stats = """
@@ -41,61 +42,64 @@ class Stats extends StatelessWidget {
       appBar: AppBar(title: Text('Stats')),
       body: Query(
         options: QueryOptions(document: gql(stats), variables: {'days': 30}),
-        builder: (QueryResult result, {
+        builder: (
+          QueryResult result, {
           Refetch? refetch,
           FetchMore? fetchMore,
         }) {
           var stats = result.data!['dashboardStatistics'];
           print(stats);
-          return Column(
+          return Column(children: [
+              TextField(
+              keyboardType: TextInputType.number,
+              textAlign: TextAlign.right
+          ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              textDirection: TextDirection.rtl,
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  textDirection: TextDirection.rtl,
-                  children: [
-                    Text(stats['routine']['title']),
-                    Text(stats['routine']['count'].toString()),
-                    Text(stats['routine']['kw'].toString()),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  textDirection: TextDirection.rtl,
-                  children: [
-                    Text(stats['project']['title']),
-                    Text(stats['project']['count'].toString()),
-                    Text(stats['project']['kw'].toString()),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  textDirection: TextDirection.rtl,
-                  children: [
-                    Text(stats['services']['title']),
-                    Text(stats['services']['count'].toString()),
-                    Text(stats['services']['kw'].toString()),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  textDirection: TextDirection.rtl,
-                  children: [
-                    Text(stats['ex']['title']),
-                    Text(stats['ex']['count'].toString()),
-                    Text(stats['ex']['kw'].toString()),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  textDirection: TextDirection.rtl,
-                  children: [
-                    Text(stats['total']['title']),
-                    Text(stats['total']['count'].toString()),
-                    Text(stats['total']['kw'].toString()),
-                  ],
-                ),
-              ]
-          );
+                Text(stats['routine']['title']),
+                Text(stats['routine']['count'].toString()),
+                Text(stats['routine']['kw'].toString()),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              textDirection: TextDirection.rtl,
+              children: [
+                Text(stats['project']['title']),
+                Text(stats['project']['count'].toString()),
+                Text(stats['project']['kw'].toString()),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              textDirection: TextDirection.rtl,
+              children: [
+                Text(stats['services']['title']),
+                Text(stats['services']['count'].toString()),
+                Text(stats['services']['kw'].toString()),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              textDirection: TextDirection.rtl,
+              children: [
+                Text(stats['ex']['title']),
+                Text(stats['ex']['count'].toString()),
+                Text(stats['ex']['kw'].toString()),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              textDirection: TextDirection.rtl,
+              children: [
+                Text(stats['total']['title']),
+                Text(stats['total']['count'].toString()),
+                Text(stats['total']['kw'].toString()),
+              ],
+            ),
+          ]);
         },
       ),
     );
