929098c5e357d877357719fd12237a8f11eeaf77
b-liva <ebammo20@gmail.com>
2021-06-27 16:52:37 +0430

    Remove Unused stuff

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:japp/screens/sales.dart';
 import 'package:japp/screens/stats.dart';
 import 'package:japp/screens/home.dart';
 import 'package:japp/screens/users.dart';
-import 'project_stats.dart';
 
 void main() {
   final HttpLink httpLink = HttpLink(
--- lib/project_stats.dart
+++
@@ -1,7 +0,0 @@
-class ProjectTypeStats {
-  String title;
-  int mw;
-  int sales;
-
-  ProjectTypeStats({required this.title, required this.mw, required this.sales});
-}
