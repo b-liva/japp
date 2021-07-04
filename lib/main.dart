543e5d5cd2acbab2f99a856aa240613a89f1ed1d
b-liva <ebammo20@gmail.com>
2021-07-04 13:06:49 +0430

    Add flutter_secure_storage package

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,16 @@ import 'package:japp/screens/sales.dart';
 import 'package:japp/screens/stats.dart';
 import 'package:japp/screens/home.dart';
 import 'package:japp/screens/users.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+//const SERVER = 'http://10.0.2.2/graphql/'
+const SERVER = 'http://crm.jemcomotor.ir/graphql/';
+final storage = FlutterSecureStorage();
+
 
 void main() {
   final HttpLink httpLink = HttpLink(
-//    'http://10.0.2.2/graphql/',
-    'http://crm.jemcomotor.ir/graphql/',
+    SERVER,
   );
 
   ValueNotifier<GraphQLClient> client = ValueNotifier(
