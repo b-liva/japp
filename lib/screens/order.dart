0ccf57c5c87aa4de73b5252bb2dcc6c0b3b6ad37
b-liva <ebammo20@gmail.com>
2021-07-14 16:38:35 +0430

    syncfusion datatable

--- lib/screens/order.dart
+++ lib/screens/order.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:syncfusion_flutter_datagrid/datagrid.dart';
 
 class Order extends StatefulWidget {
   @override
@@ -7,6 +8,16 @@ class Order extends StatefulWidget {
 }
 
 class _OrderState extends State<Order> {
+  List<Employee> employees = <Employee>[];
+  late EmployeeDataSource employeeDataSource;
+
+  @override
+  void initState() {
+    super.initState();
+    employees = getEmployeeData();
+    employeeDataSource = EmployeeDataSource(employeeData: employees);
+  }
+
   TextEditingController numberController = new TextEditingController();
   String number = '';
   String getOrder = """
@@ -59,59 +70,152 @@ class _OrderState extends State<Order> {
             var orderRes = result.data;
             var specs = result.data?['orderByNumber']['reqspecSet']['edges'];
 
-            return SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: Column(
-                children: [
-                  TextField(
-                    controller: numberController,
-                    keyboardType: TextInputType.number,
+            return Column(
+              children: [
+                TextField(
+                  controller: numberController,
+                  keyboardType: TextInputType.number,
+                ),
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      number = numberController.text;
+                    });
+                    refetch!();
+                  },
+                  child: Text('get'),
+                ),
+                Text(
+                    'شماره درخواست: ${orderRes?['orderByNumber']['number']}'),
+                Text(orderRes?['orderByNumber']['customer']['name'] ?? ''),
+                Text(orderRes?['orderByNumber']['dateFa'] ?? ''),
+                SfDataGrid(
+                  source: employeeDataSource,
+                  columns: <GridColumn>[
+                    GridColumn(
+                        columnName: 'id',
+                        label: Container(
+                            padding: EdgeInsets.all(16.0),
+                            alignment: Alignment.center,
+                            child: Text(
+                              'ID',
+                            ))),
+                    GridColumn(
+                        columnName: 'name',
+                        label: Container(
+                            padding: EdgeInsets.all(8.0),
+                            alignment: Alignment.center,
+                            child: Text('Name'))),
+                    GridColumn(
+                        columnName: 'designation',
+                        label: Container(
+                            padding: EdgeInsets.all(8.0),
+                            alignment: Alignment.center,
+                            child: Text(
+                              'Designation',
+                              overflow: TextOverflow.ellipsis,
+                            ))),
+                    GridColumn(
+                        columnName: 'salary',
+                        label: Container(
+                            padding: EdgeInsets.all(8.0),
+                            alignment: Alignment.center,
+                            child: Text('Salary'))),
+                    GridColumn(
+                        columnName: 'salary2',
+                        label: Container(
+                            padding: EdgeInsets.all(8.0),
+                            alignment: Alignment.center,
+                            child: Text('Salary2'))),
+                    GridColumn(
+                        columnName: 'salary3',
+                        label: Container(
+                            padding: EdgeInsets.all(8.0),
+                            alignment: Alignment.center,
+                            child: Text('Salary3'))),
+                  ],
+                  footerFrozenRowsCount: 1,
+                  footer: Container(
+                    child: Center(
+                      child: Text(
+                        'Footer view',
+                      ),
+                    )
                   ),
-                  TextButton(
-                    onPressed: () {
-                      setState(() {
-                        number = numberController.text;
-                      });
-                      refetch!();
-                    },
-                    child: Text('get'),
-                  ),
-                  Text(
-                      'شماره درخواست: ${orderRes?['orderByNumber']['number']}'),
-                  Text(orderRes?['orderByNumber']['customer']['name'] ?? ''),
-                  Text(orderRes?['orderByNumber']['dateFa'] ?? ''),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: DataTable(
-                      columns: [
-                        DataColumn(label: Text('ردیف')),
-                        DataColumn(label: Text('تعداد')),
-                        DataColumn(label: Text('کیلووات')),
-                        DataColumn(label: Text('دور')),
-                        DataColumn(label: Text('ولتاژ')),
-                      ],
-                      rows: List<DataRow>.generate(
-                          specs?.length ?? 0,
-                          (index) => DataRow(cells: [
-                                DataCell(Text((index + 1).toString())),
-                                DataCell(Text(
-                                    specs[index]['node']['qty'].toString())),
-                                DataCell(Text(
-                                    specs[index]['node']['kw'].toString())),
-                                DataCell(Text(specs[index]['node']['rpmNew']
-                                        ['rpm']
-                                    .toString())),
-                                DataCell(Text(specs[index]['node']['voltage']
-                                    .toString())),
-                              ])),
-                    ),
-                  ),
-                ],
-              ),
+                )
+              ],
             );
           },
         ),
       ),
     );
+  }
+
+  List<Employee> getEmployeeData() {
+    return [
+      Employee(10001, 'James', 'Project Lead', 20000, 200, 300),
+      Employee(10002, 'Kathryn', 'Manager', 30000, 200, 300),
+      Employee(10003, 'Lara', 'Developer', 15000, 200, 300),
+      Employee(10004, 'Michael', 'Designer', 15000, 200, 300),
+      Employee(10005, 'Martin', 'Developer', 15000, 200, 300),
+      Employee(10006, 'Newberry', 'Developer', 15000, 200, 300),
+      Employee(10007, 'Balnc', 'Developer', 15000, 200, 300),
+      Employee(10008, 'Perry', 'Developer', 15000, 200, 300),
+      Employee(10009, 'Gable', 'Developer', 15000, 200, 300),
+      Employee(10010, 'Grimes', 'Developer', 15000, 200, 300)
+    ];
+  }
+}
+
+class Employee {
+  /// Creates the employee class with required details.
+  Employee(this.id, this.name, this.designation, this.salary, this.salary2, this.salary3);
+
+  /// Id of an employee.
+  final int id;
+
+  /// Name of an employee.
+  final String name;
+
+  /// Designation of an employee.
+  final String designation;
+
+  /// Salary of an employee.
+  final int salary;
+  final int salary2;
+  final int salary3;
+}
+
+class EmployeeDataSource extends DataGridSource {
+  /// Creates the employee data source class with required details.
+  EmployeeDataSource({required List<Employee> employeeData}) {
+    _employeeData = employeeData
+        .map<DataGridRow>((e) => DataGridRow(cells: [
+              DataGridCell<int>(columnName: 'id', value: e.id),
+              DataGridCell<String>(columnName: 'name', value: e.name),
+              DataGridCell<String>(
+                  columnName: 'designation', value: e.designation),
+              DataGridCell<int>(columnName: 'salary', value: e.salary),
+              DataGridCell<int>(columnName: 'salary', value: e.salary2),
+              DataGridCell<int>(columnName: 'salary', value: e.salary3),
+            ]))
+        .toList();
+  }
+
+  List<DataGridRow> _employeeData = [];
+
+  @override
+  List<DataGridRow> get rows => _employeeData;
+
+  @override
+  DataGridRowAdapter buildRow(DataGridRow row) {
+    return DataGridRowAdapter(
+        cells: row.getCells().map<Widget>((e) {
+      return Container(
+        alignment: Alignment.center,
+        padding: EdgeInsets.all(8.0),
+        child: Text(e.value.toString()),
+      );
+    }).toList());
   }
 }
