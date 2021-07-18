44238a9dc493757224aa282cf105fd2cd8a50285
b-liva <ebammo20@gmail.com>
2021-07-18 14:51:56 +0430

    Add token to request

--- lib/main.dart
+++ lib/main.dart
@@ -9,20 +9,26 @@ import 'package:japp/screens/home.dart';
 import 'package:japp/screens/users.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-//const SERVER = 'http://10.0.2.2/gqlqrs/';
-//const SERVER = 'http://192.168.43.57/gqlqrs/';
-const SERVER = 'http://192.168.1.44/gqlqrs/';
-//const SERVER = 'http://crm.jemcomotor.ir/gqlqrs/';
+const VIRTUAL_SERVER = 'http://10.0.2.2/gqlqrs/';
+const CHROME_SERVER = 'http://localhost/gqlqrs/';
+const SERVER = 'http://192.168.43.57/gqlqrs/';
+const DEVICE_SERVER = 'http://192.168.1.44/gqlqrs/';
 
 void main() {
   final HttpLink httpLink = HttpLink(
     SERVER,
   );
 
+  final authLink = AuthLink(
+    getToken: () async => "JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImplbWNvMyIsImV4cCI6MTYyNjUxMDQ0Miwib3JpZ0lhdCI6MTYyNjUxMDE0Mn0.7xI59Nz-EB6yPQWOZAF7vfqKzXX9Kqod7fI2RroThhE",
+  );
+
+  var link = authLink.concat(httpLink);
+
   ValueNotifier<GraphQLClient> client = ValueNotifier(
     GraphQLClient(
       cache: GraphQLCache(),
-      link: httpLink,
+      link: link,
     ),
   );
 
