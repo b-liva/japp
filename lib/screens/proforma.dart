ac1585a7a4ce31fe906b947ddc9d10c4194aafa6
b-liva <ebammo20@gmail.com>
2021-07-19 11:01:58 +0430

    get proforma from server

--- lib/screens/proforma.dart
+++ lib/screens/proforma.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
-class ProformaArgs{
+class ProformaArgs {
   final String id;
   final int number;
 
@@ -10,16 +11,52 @@ class ProformaArgs{
 class Proforma extends StatelessWidget {
   static const routeName = '/proforma';
 
+  String getProforma = """
+  query getProforma(\$id:ID!){
+  proforma(id:\$id){
+    number
+    reqId{
+      customer{
+        name
+      }
+    }
+  }
+}
+  """;
+
   @override
   Widget build(BuildContext context) {
     final args = ModalRoute.of(context)!.settings.arguments as ProformaArgs;
     return Directionality(
       textDirection: TextDirection.rtl,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('proforma'),
+      child: Query(
+        options: QueryOptions(
+          document: gql(getProforma),
+          variables: {'id': args.id},
         ),
-        body: Text(args.number.toString()),
+        builder: (
+          QueryResult result, {
+          Refetch? refetch,
+          FetchMore? fetchMore,
+        }) {
+          if (result.isLoading) {
+            return Center(child: CircularProgressIndicator());
+          }
+          var proforma = result.data!['proforma'];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('proforma'),
+            ),
+            body: Column(
+              children: [
+//                Text(args.number.toString()),
+                Text(proforma!['number'].toString()),
+                Text(proforma!['reqId']['customer']['name']),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
