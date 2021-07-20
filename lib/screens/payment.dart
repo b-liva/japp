6e01d4b2710509cd3caee4c2cf36c092f27e1052
b-liva <ebammo20@gmail.com>
2021-07-20 09:39:28 +0430

    Add proforma to payment page

--- lib/screens/payment.dart
+++ lib/screens/payment.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:intl/intl.dart' as intl;
+import 'package:japp/screens/proforma.dart';
 
 class PaymentArgs {
   final String id;
@@ -13,22 +15,59 @@ class Payment extends StatelessWidget {
   var f = intl.NumberFormat("#,###");
   var fd = intl.NumberFormat("#.#");
 
+  String getPayment = """
+  query getPaymnet(\$id:ID!){
+  payment(id:\$id) {
+    id
+    amount
+    xprefId{
+      number
+      id
+    }
+  }
+}
+  """;
+
   @override
   Widget build(BuildContext context) {
     final args = ModalRoute.of(context)!.settings.arguments as PaymentArgs;
 
     return Directionality(
-      textDirection: TextDirection.rtl,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('payment'),
-        ),
-        body: Column(
-          children: [
-            Text("مبلغ: ${f.format(args.amount)}"),
-          ],
-        ),
-      ),
-    );
+        textDirection: TextDirection.rtl,
+        child: Query(
+          options: QueryOptions(
+            document: gql(getPayment),
+            variables: {'id': args.id},
+          ),
+          builder: (
+            QueryResult result, {
+            Refetch? refetch,
+            FetchMore? fetchMore,
+          }) {
+            if (result.isLoading) {
+              return Center(child: CircularProgressIndicator());
+            }
+            var payment = result.data!['payment'];
+
+            return Scaffold(
+              appBar: AppBar(
+                title: Text('payment'),
+              ),
+              body: Column(
+                children: [
+                  Text("مبلغ: ${f.format(payment['amount'])}"),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, Proforma.routeName,
+                          arguments:
+                              ProformaArgs(payment['xprefId']['id']));
+                    },
+                    child: Text(payment['xprefId']['number'].toString()),
+                  )
+                ],
+              ),
+            );
+          },
+        ));
   }
 }
